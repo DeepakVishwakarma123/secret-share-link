e78e5400cec9e8421a17d39088f21c506305cbd2
deepak <deepakvishwakarmajs@gmail.com>
2026-07-08 12:19:41 +0530

    get secret route add

--- backend/src/routes/Link-route.js
+++ backend/src/routes/Link-route.js
@@ -1,12 +1,12 @@
 import Router from "express"
 import GenerateLink from "../controllers/Generatelink-Controller.js"
-
+import { GetSecretandKey } from "../controllers/GetSecret-Controller.js"
 const LinkRouter=Router()
 
 //route /vanishlink/ reqeust to these routes
 
 LinkRouter.route('/generateLink').get(GenerateLink)
-
+LinkRouter.route('/getsecret/:randomid').get(GetSecretandKey)
 
 
 export default LinkRouter
