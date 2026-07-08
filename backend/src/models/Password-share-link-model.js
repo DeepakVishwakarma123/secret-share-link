e0dbcd989af97a06b134f5443aaf3bb8db02eb67
deepak <deepakvishwakarmajs@gmail.com>
2026-07-08 12:20:20 +0530

    link model add

---
+++ backend/src/models/Password-share-link-model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+import { Schema } from "mongoose"
+import { random } from "nanoid"
+
+const passwordShareLinkSchema=new Schema(
+    {   
+        userEncryptedSecret:{
+            type:String
+        },
+        privatePart:{
+            type:String
+        },
+        isViewed:{
+            type:Boolean,
+            default:false
+        },
+        RandomShortId:{
+            type:String,
+        },
+        LinkdeleteTime:{
+            type:Date,
+            expires:0,
+            default:new Date().setSeconds(180)
+        }
+    }
+)
+
+
+
+const passwordShareLink=mongoose.model('passwordsharelink',passwordShareLinkSchema)
+
+export { passwordShareLink }
+
