110122911493ab60d33299c312566333f07a6ecd
deepak <deepakvishwakarmajs@gmail.com>
2026-07-12 15:28:23 +0530

    Crypto js update

--- frontend/secret-share-link/src/Hooks/Crypto.js
+++ frontend/secret-share-link/src/Hooks/Crypto.js
@@ -1,5 +1,8 @@
 let cryptoObject=window.crypto
 let subtleObject=window.crypto.subtle
+let encoderObj=new TextEncoder()
+let decodeObject=new TextDecoder()
+
 
 async function generateKey()
 {
@@ -11,6 +14,8 @@ async function generateKey()
         true,
         ["encrypt","decrypt"]
     )
+    console.log("custome key",customKey);
+    
     return customKey
 }
 
@@ -19,16 +24,37 @@ async function EncryptData(text)
 {
     //Text encoder 
     let key=await generateKey()
-    let encoderObj=new TextEncoder()
+    let jwkjson=await subtleObject.exportKey("jwk",key)
+
+   
     let encodedText=encoderObj.encode(text)
+    
     const iv=cryptoObject.getRandomValues(new Uint8Array(12))
+    
     let cipherTextArrayBuffer=await subtleObject.encrypt({ name: "AES-GCM", iv },key,encodedText);
-     console.log("cipher text array buffer",cipherTextArrayBuffer);
-     
-    return [cipherTextArrayBuffer,iv,key]
+    let cipherTextTypedArray=new Uint8Array(cipherTextArrayBuffer)
+
+    let cipherTextBase64Url=cipherTextTypedArray.toBase64(
+   { 
+    alphabet: "base64url", 
+    omitPadding: true 
+  }
+    )
+
+     let ivTextBase64Url=iv.toBase64(
+   { 
+    alphabet: "base64url", 
+    omitPadding: true 
+  }
+    )
+  console.log("hello",cipherTextBase64Url);
+  
+    let array=[cipherTextBase64Url,ivTextBase64Url,jwkjson]
+    return array
 }
 
-let encryptedData=EncryptData("rohitgonga sdfdfddf")
+
+
 
 
 
@@ -37,9 +63,8 @@ async function DecryptData()
   let ArrayObject=await encryptedData
   let [cipherTextArrayBuffer,iv,key]=ArrayObject
   let plainTextArrayBuffer=await subtleObject.decrypt({ name: "AES-GCM", iv }, key,cipherTextArrayBuffer)
-  let decodeObject=new TextDecoder()
   let secretData=decodeObject.decode(plainTextArrayBuffer)
   console.log("the secret data is here",secretData); 
 }
 
-DecryptData()
+export {EncryptData,DecryptData,generateKey}
