dcc6d47ee34de009cea213dc6b1be9821aa13110
deepak <deepakvishwakarmajs@gmail.com>
2026-07-13 12:43:54 +0530

    View Secret Cmpnt add

---
+++ frontend/secret-share-link/src/components/DecryptSecret.jsx
@@ -0,0 +1,15 @@
+import {FaEye} from "react-icons/fa"
+import ViewSecret from "./ViewSecret"
+import NothingIsLeft from "./NothingView"
+
+function DecryptSecret()
+{
+    return (
+     <>
+     {/* <ViewSecret/> */}
+     <NothingIsLeft/>
+     </>
+    )
+}
+
+export default DecryptSecret
