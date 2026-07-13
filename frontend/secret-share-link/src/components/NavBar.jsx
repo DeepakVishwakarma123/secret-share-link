8602d301fc2ae2c7dafd7fe1051aaaaa43c5cdbf
deepak <deepakvishwakarmajs@gmail.com>
2026-07-13 12:44:59 +0530

    Nabar Add

---
+++ frontend/secret-share-link/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+function NavBar()
+{
+    return (
+        <ul className="flex justify-around p-4 uppercase font-semibold bg-green-200">
+            <li>
+                <a href="">Secret Share</a>
+            </li>
+            <li>
+                <a href="">Github</a>
+            </li>
+            <li>
+                <a href="">X</a>
+            </li>
+            <li>
+                <a href="">YOUTUBE</a>
+            </li>
+        </ul>
+    )
+}
+
+export default NavBar
