ff7e1f2807c4fb215d8bd80533388af4f2ac74d5
deepak <deepakvishwakarmajs@gmail.com>
2026-07-13 12:44:25 +0530

    Footer Add

---
+++ frontend/secret-share-link/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+function Footer()
+{
+    return (
+        <ul className="flex flex-col items-center justify-around h-[32vh] p-2   uppercase font-semibold bg-amber-200">
+           
+           <li>
+            <a href="">(c) ALL RIGHTS RESERVED 2026</a>
+           </li>
+            <li>
+                <a href="">Privacy Policy</a>
+            </li>
+            <li>
+                <a href="">AES GCM</a>
+            </li>
+            <li>
+                <a href="">HOW SECRET SHARE WORK</a>
+            </li>
+            <li>
+                <a href="">RESOURCES</a>
+            </li>
+        </ul>
+    )
+}
+
+export default Footer
