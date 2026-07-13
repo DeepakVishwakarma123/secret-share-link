5f36c09083737d28a7856bcb7f2efa13fb4748d3
deepak <deepakvishwakarmajs@gmail.com>
2026-07-13 12:45:57 +0530

    realease update add

---
+++ frontend/secret-share-link/src/components/Updates.jsx
@@ -0,0 +1,25 @@
+function Updates()
+{
+    return <>
+    <>
+    <marquee behavior="" scrollamount="12" direction="left" className="p-4"> 
+    <div className="flex gap-10  font-semibold">
+    Coming Soon!!!
+    <span className="text-red-300">
+    Custom Times For Expiry Of link
+    </span>
+
+    <span className="text-yellow-500">
+     Passpharse Option For Link
+    </span>
+    
+    <span className="text-orange-600">
+        Set Opening View Count For Link
+    </span>
+    </div>
+    </marquee>
+    </>
+    </>
+}
+
+export default Updates
