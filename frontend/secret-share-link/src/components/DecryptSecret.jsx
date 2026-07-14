b0f85b744b7850f6aa0926e9edd5d2bb5c177d99
deepak <deepakvishwakarmajs@gmail.com>
2026-07-14 18:07:42 +0530

    conditional rendering add,data fetching add via useeffect

--- frontend/secret-share-link/src/components/DecryptSecret.jsx
+++ frontend/secret-share-link/src/components/DecryptSecret.jsx
@@ -1,15 +1,48 @@
 import {FaEye} from "react-icons/fa"
 import ViewSecret from "./ViewSecret"
 import NothingIsLeft from "./NothingView"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { GetDataFromApi } from "../Hooks/fetchApi"
+import { DecryptData } from "../Hooks/Crypto"
+import { useLocation } from "react-router-dom"
 
 function DecryptSecret()
-{
+{   
+   let {linkid}=useParams()
+   let {hash}=useLocation()
+   const [showData,setData]=useState(null)
+   useEffect(
+    () => {
+        let windowlocation=window.location
+        console.log(windowlocation);
+        
+    GetDataFromApi(`http://localhost:4000/secure/getsecret/${linkid}`).then(
+        (data) => {
+            if(data["isDeleted"])
+            {
+                  setData(null)
+            }
+            else{
+                setData(data)
+            }  
+        }
+    ).catch(
+        (err) => {
+            console.error(err)
+        }
+    )
+     }
+    ,[]
+   )
     return (
      <>
-     {/* <ViewSecret/> */}
-     <NothingIsLeft/>
+     {
+     showData?<ViewSecret data={showData} hash={hash}/>:<NothingIsLeft/>
+     }
      </>
     )
 }
+
 
 export default DecryptSecret
